cce667cef2116e91ad3546ed1669521ec40e1014
Clayman <Clayman@DESKTOP-DM8E77U>
2023-09-26 13:47:44 +0300

    DataTypes

---
+++ Introduction/DataTypes/Program.cs
@@ -0,0 +1,71 @@
+﻿//#define LOGICAL_TYPES
+//#define CHARACTER_TYPES
+//#define INTEGRAL_TYPES
+#define FLOATING_TYPES
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+	class Program
+	{
+		static readonly string delimiter = "\n----------------------------------------------\n";
+		static void Main(string[] args)
+		{
+			//Object
+#if LOGICAL_TYPES
+			Console.WriteLine("BOOL:");
+			Console.WriteLine(sizeof(bool));    //Boolean
+			Console.WriteLine(true.GetType());
+			Console.WriteLine((3 + 2.5).GetType());
+			Console.WriteLine(delimiter);
+#endif
+
+#if CHARACTER_TYPES
+			Console.WriteLine("CHAR:"); //Хранит один символ в кодировке Unicode
+			Console.WriteLine(sizeof(char));
+			Console.WriteLine((int)(char.MinValue));
+			Console.WriteLine((int)(char.MaxValue));
+			Console.WriteLine(delimiter);
+#endif
+
+#if INTEGRAL_TYPES
+			Console.WriteLine("SHORT");
+			Console.WriteLine(sizeof(ushort));
+			Console.WriteLine("USHORT:\t" + ushort.MinValue + " ... " + ushort.MaxValue);
+			Console.WriteLine(" SHORT:\t" + short.MinValue + " ... " + short.MaxValue);
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine("INT");
+			Console.WriteLine(sizeof(int));
+			Console.WriteLine("UINT:\t" + uint.MinValue + " ... " + uint.MaxValue);
+			Console.WriteLine("INT:\t" + int.MinValue + " ... " + int.MaxValue);
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine("LONG:");
+			Console.WriteLine(sizeof(long));
+			Console.WriteLine("ULONG:\t" + ulong.MinValue + " ... " + ulong.MaxValue);
+			Console.WriteLine("ULONG:\t" + long.MinValue + " ... " + long.MaxValue); 
+#endif
+
+#if FLOATING_TYPES
+			//float
+			//double
+			//deciaml
+			Console.WriteLine("FLOAT:\t" + sizeof(float) + "Bytes");
+			Console.WriteLine(float.MinValue + " ... " + float.MaxValue);
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine("DOUBLE:\t" + sizeof(double) + " Bytes");
+			Console.WriteLine(double.MinValue + " ... " + double.MaxValue);
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine("Decimal:\t" + sizeof(decimal) + " Bytes");
+#endif
+			Console.ReadLine();
+		}
+	}
+}
--- ReadMe.txt
+++ ReadMe.txt
@@ -2,6 +2,12 @@ https://github.com/okovtun/PD_212_CS.git
 https://www.youtube.com/watch?v=8n51o1zBLc8&list=PLeqyOOqxeiINUQTE7hWwqUgTah8WrW5HE
 
 TODO:
+1. Решить следующие задачи:	DONE
+	https://github.com/okovtun/PU_211_CS/blob/master/Introduction/VariablesTask.pdf
+2. Написать калькулятор, который принимает выражение вида 2 + 3, и выводит результат этого выражения:
+	2 + 3 = 5;
+
+DONE:
 1. Написать консольный Shooter;
 2. Вывести на экран следующие фигуры:
 	https://github.com/okovtun/BV_011/blob/master/ControlStructures/Geometria/Task/Geometria1.txt
