aa714ee55bbf0c1dd9c47803c69dc1dec394345a
Clayman <Clayman@DESKTOP-DM8E77U>
2023-09-29 18:43:28 +0300

    Transfer parameters

---
+++ Introduction/Enumerations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerations
+{
+	class Program
+	{
+		static readonly string delimiter = "\n--------------------------------------\n";
+		static void Main(string[] args)
+		{
+			DayOfWeek day = DayOfWeek.Friday;
+			Console.WriteLine(day);
+			string[] dayName = Enum.GetNames(typeof(DayOfWeek));
+			for (int i =0; i<dayName.Length; i++)
+			{
+				Console.WriteLine(i + "\t" + dayName[i]);
+			}
+			Console.WriteLine(delimiter);
+
+			DistanceFromSun dfs = DistanceFromSun.Earth;
+			Console.WriteLine(dfs.GetHashCode());
+			string[] distNames = Enum.GetNames(typeof(DistanceFromSun));
+			ulong[] distValues = (ulong[])Enum.GetValues(typeof(DistanceFromSun));
+			for (int i = 0; i < distNames.Length; i++)
+			{
+				Console.WriteLine($"{distNames[i]} \t {distValues[i]}");
+			}
+			Console.WriteLine((Enum.GetUnderlyingType(typeof(DistanceFromSun))).FullName);
+		}
+		enum DayOfWeek { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+		enum DistanceFromSun : ulong
+		{
+			Sun = 0,
+			Mercury = 57900000,
+			Venus	= 108200000,
+			Earth	= 149600000
+		};
+	}
+}
---
+++ Introduction/TransferParameters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferParameters
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a;
+			int b;
+			Input(out a, out b);
+			Console.WriteLine($"{a}\t{b}");
+			Exchange(ref a, ref b);
+			Console.WriteLine($"{a}\t{b}");
+		}
+		static void Input(out int a, out int b)
+		{
+			Console.Write("Введите первое число: "); a = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Введите второе число: "); b = Convert.ToInt32(Console.ReadLine());
+		}
+		static void Exchange(ref int a, ref int b)
+		{
+			int buffer = a;
+			a = b;
+			b = buffer;
+		}
+		//ref - передаваемые переменные обязательно должны быть проинициализированы
+		//out - скорее всего функция инициализирует принимаемые переменные, поэтому их можно передавать без предварительной инициализации
+	}
+}
