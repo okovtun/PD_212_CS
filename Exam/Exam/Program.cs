2c8e504566bfa34006938a8cdc7b2ba57c8a59d7
Clayman <Clayman@DESKTOP-DM8E77U>
2023-10-18 16:23:30 +0300

    Exam

---
+++ Exam/Exam/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Object[] arr = new Object[] { 2.5, 5, "Hello", true };
+			foreach (Object i in arr)
+			{
+				Console.WriteLine(i);
+			}
+		}
+	}
+}
